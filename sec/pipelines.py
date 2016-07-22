3db126e463268512328d396a1a2a4e7811ec4964
freefood89 <freefood89@gmail.com>
2016-07-22 01:54:34 +0000

    scraps and crawls

--- sec/pipelines.py
+++ sec/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
+import datetime
 
 class SecPipeline(object):
     def process_item(self, item, spider):
-        return item
+        try:
+            assert 'date' in item and len(item['date']) > 0
+            assert 'filing' in item and len(item['date']) > 0
+            assert 'date' in item and len(item['date']) > 0
+        except AssertionError as e:
+            raise DropItem('A field is missing from %s' % item)
+        else:
+            return item
--- sec/spiders/sec_spider.py
+++ sec/spiders/sec_spider.py
@@ -1,17 +1,16 @@
 import scrapy
 
 from sec.items import SecItem
+import re
 
 class SecSpider(scrapy.Spider):
     name = 'sec'
-    allowed_domains = 'sec.gov'
+    allowed_domains = ['sec.gov']
     start_urls = [
         'https://www.sec.gov/cgi-bin/browse-edgar?CIK=t&owner=exclude&action=getcompany&count=100',
     ]
 
     def parse(self, response):
-        filename = 'tmp.html'
-
         for sel in response.xpath('//table[@class="tableFile2"]/tr'):
             item = SecItem()
             item['filing'] = sel.xpath('td[1]/text()').extract()
@@ -19,3 +18,10 @@ class SecSpider(scrapy.Spider):
             item['date'] = sel.xpath('td[4]/text()').extract()
             print(item)
             yield item
+
+        next_page = response.xpath("//input[@type='button']/@onclick")
+        # print(next_page)
+        if next_page:
+            path = re.findall("'((?:.|\n)*?)'", next_page.pop().extract()).pop()
+            url = 'https://www.sec.gov' + path
+            yield scrapy.Request(url, self.parse)
